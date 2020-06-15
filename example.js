52434572a2dc8793348b8fcd258ef1f4b7d54286
Daniel Dyla <dyladan@users.noreply.github.com>
2020-06-15 17:44:43 -0400

    generate scenes in parallel

--- example.js
+++ example.js
@@ -1,22 +1,17 @@
 const { Elmo } = require('./lib');
 
 (async () => {
-    const elmo = new Elmo({ videoFile: "./example.mp4", subtitles: false });
+    const elmo = new Elmo({ videoFile: "./example.mp4", subtitles: true });
 
-    const wikiScene = await elmo.startScene('https://www.wikipedia.org/');
+    await Promise.all([
+        makeWikiScene(elmo),
+        makeDdgScene(elmo)
+    ]);
 
-    await wikiScene.waitForSelector('#searchInput');
-    await wikiScene.startRecording();
-    await wikiScene.moveTo('#searchInput');
-    await wikiScene.say("first I will search for something")
-    await wikiScene.sleep(1000);
-    await wikiScene.click();
-    await wikiScene.type("drink bleach kill virus or no");
-    await wikiScene.moveTo(".pure-button-primary-progressive")
-    await wikiScene.click();
-    await wikiScene.say("thought for sure this would return something");
-    await wikiScene.stop();
+    await elmo.build();
+})();
 
+async function makeDdgScene(elmo) {
     const ddgScene = await elmo.startScene('https://duckduckgo.com');
     await ddgScene.waitForSelector("#search_form_input_homepage");
     await ddgScene.startRecording();
@@ -28,6 +23,21 @@ const { Elmo } = require('./lib');
     await ddgScene.waitForSelector("#links");
     await ddgScene.say("guess not");
     await ddgScene.stop();
+}
 
-    await elmo.build();
-})();
+async function makeWikiScene(elmo) {
+    const wikiScene = await elmo.startScene('https://www.wikipedia.org/');
+
+    await wikiScene.waitForSelector('#searchInput');
+    await wikiScene.startRecording();
+    await wikiScene.moveTo('#searchInput');
+    await wikiScene.say("first I will search for something");
+    await wikiScene.sleep(1000);
+    await wikiScene.click();
+    wikiScene.say("drink bleach kill virus or no");
+    await wikiScene.type("drink bleach kill virus or no");
+    await wikiScene.moveTo(".pure-button-primary-progressive");
+    await wikiScene.click();
+    await wikiScene.say("thought for sure this would return something");
+    await wikiScene.stop();
+}
